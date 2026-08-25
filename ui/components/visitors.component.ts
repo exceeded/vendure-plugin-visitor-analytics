1cb14322ca6e65ef60206362044bf4fb86b75bce
Wayne Garrison <wayne@garrison.me.uk>
2026-08-25 16:22:33 +0000

    ui: point the update banner's What's-new link at the plugin changelog page

--- ui/components/visitors.component.ts
+++ ui/components/visitors.component.ts
@@ -95,7 +95,7 @@ interface VisitorProfile {
                 <div class="lic-actions">
                     <button class="gbtn gbtn-primary gbtn-sm" *ngIf="licMeta?.selfUpdate?.allowed" (click)="runSelfUpdate()" [disabled]="updating">{{ updating ? updateProgress : 'Update now' }}</button>
                     <button class="gbtn gbtn-outline gbtn-sm" (click)="copyUpdateCmd()">{{ cmdCopied ? 'Copied ✓' : 'Copy command' }}</button>
-                    <a href="https://huloglobal.com/vendure-plugins/visitor-analytics/" target="_blank" class="gbtn gbtn-outline gbtn-sm">What&rsquo;s new ↗</a>
+                    <a href="https://huloglobal.com/vendure-plugins/visitor-analytics/changelog/" target="_blank" class="gbtn gbtn-outline gbtn-sm">What&rsquo;s new ↗</a>
                     <button class="gbtn gbtn-outline gbtn-sm" (click)="updateDismissed = true">Dismiss</button>
                 </div>
             </div>
